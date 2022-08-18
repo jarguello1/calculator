3ff563377f2e6f18523b17e9340e9c3105d54ec1
jarguello1 <jcarguello11@live.com>
2022-08-18 11:47:22 -0400

    Assign numberButton to function to all numbers
    Created an array for all the number buttons. Added the numberButton function to all buttons
    using for loop and the numberText array.
    
    Added additional logic to the numberButton function incase the current number is a lone
    zero.

--- script.js
+++ script.js
@@ -10,12 +10,21 @@ function clearDisplay()  {
 
 function numberButton(e) {
     let number = e.target.textContent;
-    displayValue += number;
-    display.textContent = displayValue;
+    if (displayValue === "0") {
+        displayValue = number;
+        display.textContent = displayValue;
+    } else if (displayValue !== "0") {
+        let number = e.target.textContent;
+        displayValue += number;
+        display.textContent = displayValue;
+    }
 }
 
 const clear = document.getElementById('clear');
 clear.addEventListener('click', clearDisplay);
 
-const one = document.getElementById('one');
-one.addEventListener('click', numberButton)
+let numberText = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+numberText.forEach(function(e) {
+    const numberBtn = document.getElementById(e);
+    numberBtn.addEventListener('click', numberButton);
+});
