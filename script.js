dcac09b63c670c78d82c45e1e8e01d913b319a5c
jarguello1 <jcarguello11@live.com>
2022-08-18 10:55:00 -0400

    Add display value, clear, and number functions
    Create displayValue variable that holds the numbers displayed. Assign it to the display container.
    Create clearDisplay functions which clears whatever values are in the displayValue variable. Assign it
    to the 'AC' button. Created the numberButton function which adds a number to the displayValue variable,
    whent that button is pressed.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let displayValue = "";
 
+const display = document.querySelector('#display');
+display.textContent = displayValue;
+
+function clearDisplay()  {
+    displayValue = "";
+    display.textContent = displayValue;
+}
+
+function numberButton(e) {
+    let number = e.target.textContent;
+    displayValue += number;
+    display.textContent = displayValue;
+}
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', clearDisplay);
+
+const one = document.getElementById('one');
+one.addEventListener('click', numberButton)
